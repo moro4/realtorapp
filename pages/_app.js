b5273fe7a550e6dd06075879e6ae0c1a2cddae3f
zepeersGily <>
2022-09-09 17:09:17 +0200

    implemented a semantic wrapper around the main content

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import {ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
    return (
-      <ChakraProvider>
-         <Component {...pageProps} />
-      </ChakraProvider>
+      <>
+         <Head>
+         </Head>
+         <ChakraProvider>
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
+         </ChakraProvider>
+      </>
    )
 }
 
-export default MyApp
+export default MyApp;
